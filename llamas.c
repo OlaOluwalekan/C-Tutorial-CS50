6d773d10f05e3ea3808adca7a5aa323105904eee
OlaOluwalekan <81148417+OlaOluwalekan@users.noreply.github.com>
2023-09-06 16:35:10 +0000

    Wed, Sep 6, 2023, 5:35 PM +01:00

--- llamas.c
+++ llamas.c
@@ -8,11 +8,11 @@ int main(void)
   int i = 1;
   // int po = 0;
 
-  // while (i <= year) {
-  //   po = (13 * startPo)/12;
-  //   startPo = po;
-  // }
+  while (i <= year) {
+    startPo = (13 * startPo) / 12;
+    i++;
+  }
 
-  printf("Population of llamas after %i years is %i ", year, po);
+  printf("Population of llamas after %i years is %i ", year, startPo);
   printf("\n");
 }
