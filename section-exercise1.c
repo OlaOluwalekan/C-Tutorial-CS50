0e71f1439d593976d514dd0d55b5dcb84b654613
OlaOluwalekan <81148417+OlaOluwalekan@users.noreply.github.com>
2023-09-06 12:35:10 +0000

    Wed, Sep 6, 2023, 1:35 PM +01:00

--- section-exercise1.c
+++ section-exercise1.c
@@ -4,5 +4,7 @@
 int main(void)
 {
   string name = get_string("What is your name? ");
-  printf("Your name is %s\n", name)
+  printf("Your name is %s\n", name);
+
+  int age = get_int("how hold are you? ");
 }
