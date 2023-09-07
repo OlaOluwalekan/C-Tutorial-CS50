354e9c6eb541c2a30a13ea6712fcc4b120b932c8
OlaOluwalekan <81148417+OlaOluwalekan@users.noreply.github.com>
2023-09-07 08:13:14 +0000

    Thu, Sep 7, 2023, 9:13 AM +01:00

--- llamas.c
+++ llamas.c
@@ -24,16 +24,16 @@ int main(void)
 
   // startPo = round(startPo / 12);
 
-  printf("%i : %f", (int)startPo, unrounded);
+  // printf("%i : %f", (int)startPo, unrounded);
 
   while (startPo < targetPo)
   {
-      startPo += round(startPo / 4);
-      startPo -= round(startPo / 3);
+      startPo += round(startPo / 12);
+      // startPo -= round(startPo / 3);
       year++;
   }
 
   // printf("Population of llamas after %i years is %f ", year, startPo);
-  // printf("Number of years for llamas to reach %i population is: %i", targetPo, year);
+  printf("Number of years for llamas to reach %i population is: %i", targetPo, year);
   printf("\n");
 }
