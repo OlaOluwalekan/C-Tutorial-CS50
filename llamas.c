20af178936aacc820f2386b27c732873949c4f00
OlaOluwalekan <81148417+OlaOluwalekan@users.noreply.github.com>
2023-09-07 07:12:53 +0000

    Thu, Sep 7, 2023, 8:12 AM +01:00

--- llamas.c
+++ llamas.c
@@ -11,13 +11,13 @@ int main(void)
   while (startPo <= targetPo)
   {
       float pow = 1;
-      for (int j = 1; j <= i; j++)
+      for (int j = 0; j <= year; j++)
       {
           pow *= (13 / 12);
       }
       startPo = pow * startPo;
       printf("%i", startPo);
-      i++;
+      year++;
   }
 
   // do {
